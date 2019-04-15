beb52edfa6865e4afc5461afbdd743e9fa609a02
FirstEye <49639515+FirstEye@users.noreply.github.com>
2019-04-15 17:55:30 +0800

    Add files via upload

---
+++ 源1.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include"БъЭЗ.h"
+using namespace std;
+
+ArrayMaxSum::ArrayMaxSum()
+{
+	cin >> number;
+	if (number > 0)
+	{
+		array_ = new int[10];
+		for (int i = 0; i < number; i++)
+		{
+			cin >> *(array_ + i);
+		}
+	}
+	else
+	{
+		number = 0;
+		array_ = NULL;
+	}
+}
+ArrayMaxSum::ArrayMaxSum(int* a, int n)
+{
+	array_ = new int[10];
+	for (int i = 0; i < n; i++)
+	{
+		array_[i] = a[i];
+	}
+	number = n;
+}
+ArrayMaxSum::~ArrayMaxSum()
+{
+	delete[]array_;
+	array_ = NULL;
+}
+int ArrayMaxSum::getArrayMaxSum()
+{
+	if (array_ == NULL)return 0;
+	int tempsum = array_[0];
+	int maxsum = 0;
+	for (int i = 1; i < number; i++)
+	{
+		tempsum = tempsum + array_[i] > array_[i] ? tempsum + array_[i] : array_[i];
+		maxsum = tempsum > maxsum ? tempsum : maxsum;
+	}
+	return maxsum;
+}
